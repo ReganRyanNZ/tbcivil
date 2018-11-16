c83c16bbb811890a334e11a1e1f75c74316b80f6
Regan Ryan <regan.ryan.nz@gmail.com>
2018-11-16 15:29:44 +1000

    minor tidyup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
     website = "https://#{website}" unless website[/^http/]
     tmp = Tempfile.new("pdf-chrome-puppeteer")
     system("yarn createPDF #{Shellwords.escape(website)} #{Shellwords.escape(tmp.path)}")
-    # pdf_data = File.read(tmp.path)
     pdf_filename = "output.pdf"
     send_file(tmp.path, filename: pdf_filename, type: 'application/pdf', disposition: 'attachment')
   end
